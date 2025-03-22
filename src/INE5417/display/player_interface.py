165f628e524f548fcb6c7690d280fe2d28f4d971
João Gabriel Feres <joaogabrielferes@gmail.com>
2025-03-22 11:35:09 -0300

    wip

--- src/INE5417/display/player_interface.py
+++ src/INE5417/display/player_interface.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox, simpledialog
+from PIL import ImageTk, Image
 from tkinter import ttk
 
 from ttkthemes import ThemedTk
@@ -7,21 +8,24 @@ from ttkthemes import ThemedTk
 from src.INE5417.dog.dog_actor import DogActor
 from src.INE5417.dog.dog_interface import DogPlayerInterface
 from src.INE5417.logic.board import Board
-from src.INE5417.utils.constants import GAME_NAME, WINDOW_GEOMETRY, RESOURCES_DIR, FONT
+from src.INE5417.utils.constants import GAME_NAME, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_GEOMETRY, RESOURCES_DIR, FONT, \
+    BOARD_WIDTH, BOARD_HEIGHT, BOARD_FACTOR
 from src.INE5417.utils.game_state import GameState
 
 
 class PlayerInterface(DogPlayerInterface):
     def __init__(self) -> None:
         super().__init__()
-        self.dog: DogActor = DogActor()
-        self.root: tk.Tk = ThemedTk(theme="Equilux")
-        # self.root: tk.Tk = ThemedTk(theme="Adapta")
+        self.root: tk.Tk = tk.Tk()
+        s = ttk.Style(self.root)
+        s.theme_use("clam")
+        s.configure("flat.TButton", borderwidth=0, bg="")
         self.board: Board = Board()
-        self.player_name: str = self.get_player_name()
-        self.assets: dict[str, tk.PhotoImage] = self.load_assets()
-        self.menu_file: tk.Menu
+        self.assets: dict[str, ImageTk.PhotoImage] = self.load_assets()
         self.populate_window()
+        self.menu_file: tk.Menu = self.initialize_menubar()
+        self.player_name: str = self.get_player_name()
+        self.dog: DogActor = DogActor()
         self.initialize_dog()
         self.update_game_state()
         self.root.mainloop()
@@ -39,8 +43,8 @@ class PlayerInterface(DogPlayerInterface):
         extension = ".png"
 
         board_file = RESOURCES_DIR / f"board{extension}"
-
-        assets["board"] = tk.PhotoImage(file=board_file)
+        board_image = Image.open(board_file).resize((BOARD_WIDTH, BOARD_HEIGHT))
+        assets["board"] = ImageTk.PhotoImage(board_image)
 
         # assets dos triângulos
         # for i in range(12):
@@ -51,11 +55,13 @@ class PlayerInterface(DogPlayerInterface):
         for i in range(6):
             color_a = "red" + str(i)
             color_a_file = RESOURCES_DIR / f"{color_a}{extension}"
-            assets[color_a] = tk.PhotoImage(file=color_a)
+            color_a_image = Image.open(color_a_file).resize((int(WINDOW_WIDTH * 0.09), int(WINDOW_HEIGHT * 0.09)))
+            assets[color_a] = ImageTk.PhotoImage(color_a_image)
 
             color_b = "black" + str(i)
             color_b_file = RESOURCES_DIR / f"{color_b}{extension}"
-            assets[color_b] = tk.PhotoImage(file=color_b_file)
+            color_b_image = Image.open(color_b_file).resize((int(WINDOW_WIDTH * 0.09), int(WINDOW_HEIGHT * 0.09)))
+            assets[color_b] = ImageTk.PhotoImage(color_b_image)
 
         return assets
 
@@ -69,8 +75,8 @@ class PlayerInterface(DogPlayerInterface):
         # logo_frame = ttk.Frame(self.root)
         # logo_frame.pack(fill=tk.X, side = tk.TOP, expand=True)
 
-        board_frame = ttk.Frame(self.root)
-        board_frame.pack(fill=tk.X, side = tk.TOP, expand=True)
+        game_frame = ttk.Frame(self.root)
+        game_frame.pack(fill=tk.X, side = tk.TOP, expand=True)
 
         message_frame = ttk.Frame(self.root)
         message_frame.pack(fill=tk.BOTH, side = tk.TOP, expand=False)
@@ -79,28 +85,51 @@ class PlayerInterface(DogPlayerInterface):
         # logo = tk.PhotoImage(file="images/logo.png")
         # ttk.Label(self.logo_frame, image=self.logo).grid(row=0, column=0)
 
+        canvas_board = tk.Canvas(game_frame, width=BOARD_WIDTH, height=BOARD_HEIGHT)
+        canvas_board.create_image(BOARD_WIDTH // 2, BOARD_HEIGHT // (2 - 0.15), image=self.assets["board"])
+        triangles_rel_coordinates = (
+            (0.495, 0.15),
+        )
+        for i in range(1):
+            relx, rely = triangles_rel_coordinates[i]
+            factor = 0.01
+            button_triangle = tk.Button(canvas_board, width=int(BOARD_WIDTH * factor), height=int(BOARD_WIDTH * factor * 11/20), bg=self.root.cget("bg"), borderwidth=0)
+            button_triangle.place(relx=relx, rely=rely)
+
+        canvas_board.grid(row=0, column=2, sticky=tk.NSEW, rowspan=6)
+
+        for row in range(6):
+            button1_stone_colora = ttk.Button(game_frame, image=self.assets[f"red{row}"], style='flat.TButton')
+            button2_stone_colora = ttk.Button(game_frame, image=self.assets[f"red{row}"], style='flat.TButton')
+            button1_stone_colorb = ttk.Button(game_frame, image=self.assets[f"black{row}"], style='flat.TButton')
+            button2_stone_colorb = ttk.Button(game_frame, image=self.assets[f"black{row}"], style='flat.TButton')
+            button1_stone_colora.grid(row=row, column=0, sticky=tk.W, padx=0, pady=0)
+            button2_stone_colora.grid(row=row, column=1, sticky=tk.W, padx=0, pady=0)
+            button1_stone_colorb.grid(row=row, column=3, sticky=tk.E, padx=0, pady=0)
+            button2_stone_colorb.grid(row=row, column=4, sticky=tk.E, padx=0, pady=0)
+
         message_label = ttk.Label(message_frame, text=GAME_NAME, font=FONT)
         message_label.grid(row=0, column=1)
 
-        self.initialize_menubar()
 
-    def initialize_menubar(self) -> None:
+    def initialize_menubar(self) -> tk.Menu:
         menubar = tk.Menu(self.root)
         menubar.option_add('*tearOff', tk.FALSE)
         self.root['menu'] = menubar
 
-        self.menu_file = tk.Menu(menubar, bg="#FFFFFF")
-        menubar.add_cascade(menu=self.menu_file, label="Menu")
+        menu_file = tk.Menu(menubar, bg="#FFFFFF")
+        menubar.add_cascade(menu=menu_file, label="Menu")
 
         # menu_file.add_command(label="Regras", command= self.rules_window_child,
         #                            activebackground="#A7CCE7", activeforeground="#000")
-        self.menu_file.add_command(label="Iniciar partida", command=self.start_match,
+        menu_file.add_command(label="Iniciar partida", command=self.start_match,
                                    activebackground="#A7CCE7", activeforeground="#000")
-        self.menu_file.add_command(label="Reiniciar jogo", command=self.start_game,
+        menu_file.add_command(label="Reiniciar jogo", command=self.start_game,
                                    activebackground="#A7CCE7", activeforeground="#000")
-        self.menu_file.add_separator()
-        self.menu_file.add_command(label="Sair", command=self.end_window,
+        menu_file.add_separator()
+        menu_file.add_command(label="Sair", command=self.end_window,
                                    activebackground="#EA9E9E", activeforeground="#000")
+        return menu_file
 
 
     def initialize_dog(self) -> None:
--- src/INE5417/utils/constants.py
+++ src/INE5417/utils/constants.py
@@ -2,6 +2,11 @@ from pathlib import Path
 
 GAME_NAME: str = "Qyshinsu"
 VERSION: str = "0.1"
-WINDOW_GEOMETRY: str = "800x600"
+WINDOW_WIDTH: int = 1000
+WINDOW_HEIGHT: int = 800
+WINDOW_GEOMETRY: str = f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}"
+BOARD_FACTOR: float = 0.587
+BOARD_WIDTH: int = int(WINDOW_WIDTH * BOARD_FACTOR)
+BOARD_HEIGHT: int = int(WINDOW_HEIGHT * BOARD_FACTOR)
 RESOURCES_DIR: Path = Path(__file__).parent.parent.parent / "resources"
 FONT: str = "Arial 12 bold"
