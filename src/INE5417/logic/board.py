e151f86a09fa4f0372a678247493f8dad352f193
Hélcio Valentim de Andrade Neto <valentimhelcio@gmail.com>
2025-04-03 17:55:37 -0300

    wip

--- src/INE5417/logic/board.py
+++ src/INE5417/logic/board.py
@@ -14,8 +14,8 @@ class Board:
         self.local_player: Player = Player(COLOR_A)
         self.remote_player: Player = Player(COLOR_B)
         self.game_state: GameState = GameState.TITLE
-        self.regular_move: bool = True
-        self.last_positioned_stone: Stone | None = None
+        # self.regular_move: bool = True
+        self.last_move_info: None | tuple[int, Triangle] = None
         self.selected_stone: Stone | None = None
         self.triangles: list[Triangle] = self.initialize_triagles()
 
@@ -35,29 +35,79 @@ class Board:
         self.remote_player: Player = Player(self.remote_player.get_color())
         self.triangles = self.initialize_triagles()
 
-    def stone_selected(self, stone_color: str, stone_value: int) -> list[int]:
-        player = self.local_player if stone_color == self.local_player.get_color() else self.remote_player
-        self.selected_stone = player.get_stone(stone_value)
-        print("pedra selecionada", self.selected_stone.get_value())
-        if self.last_positioned_stone is None:  # primeiro move
-            return list(range(12))
-        else:  # moves posteriores
-            # TODO: esquematizar uma forma relação direta entre a pedra posicionada anteriormente e seu triângulo
-            last_positioned_stones_triangle_index = 0  # apenas para o analisador sintático não reclamar de variável sem valor
-            for i, triangle in enumerate(self.triangles):
-                if triangle.get_stone() == self.last_positioned_stone:
-                    last_positioned_stones_triangle_index = i
-                    break
-            if stone_value > 0:
-                possible_triangles_indexes = [
-                    (last_positioned_stones_triangle_index - stone_value) % 12,
-                    (last_positioned_stones_triangle_index + stone_value) % 12
-                ]
-                return [i for i in possible_triangles_indexes if self.triangles[i].get_stone() is None]
-            else:
-                # TODO: tratar caso especial da pedra zero
-                return []
+    def calculate_range(self, last_move_info: None | tuple[int, int]) -> set[Triangle]:
+        if last_move_info is None: return set(self.triangles)
+        value, position = last_move_info
+        if value == 0:
+            for i in range(1, 12):
+                var = set()
+                left = self.triangles[(position - i) % 12]
+                right = self.triangles[(position + i) % 12]
+                if left.is_free(): var.add(left)
+                if right.is_free(): var.add(right)
+                if len(var): return var
+            return set()
+        else:
+            return {
+                self.triangles[(position - value) % 12],
+                self.triangles[(position + value) % 12]
+            }
 
+    def generate_valid_triangles(self, candidate_moves: set[Triangle]) -> list[Triangle]:
+        return [
+            t for t in candidate_moves if t.is_empty() or t.get_stone().get_color() == self.local_player.get_color()
+        ]
+        #for t in candidate_moves: 
+        #    if t.is_empty() or t.get_stone().get_color() == self.local_player.get_color():
+        #        yield t 
+
+    def generate_valid_move_list(self, last_move_info: None | tuple[int, Triangle]) -> list[Triangle]:
+        candidate_moves = self.calculate_range(last_move, info)
+        valid_triangles = self.generate_valid_triangles(candidate_moves)
+        return valid_triangles
+
+    def update_move_info(self, selected_triangle: int):
+        self.last_move_info = (self.selected_stone, selected_triangle])
+        self.selected_stone = None
+
+    def execute_move(self, selected_triangle_index: int) -> dict[str, str]:
+        # atualizar self.last_move_info aqui?
+        self.update_move_info(self.triangles[selected_triangle_index])
+        move_type = self.decide_move_type()
+        if move_type == INSERT:
+            self.insert_stone(selected_triangle_index)
+        else:
+            self.remove_stone(selected_triangle_index)
+        return self.generate_dog_food()
+
+    def decide_move_type(self, selected_stone: Stone) -> MoveType:
+        if selected_stone.get_color() != self.local_player.get_color():
+            return MoveType.ASK_AGAIN
+        if selected_stone.on_board():
+            return MoveType.REMOVE
+        counter = 0
+        for t in self.triangles:
+            if t.get_stone().get_value() == selected_stone.get_value():
+                counter += 1
+        if counter < 2:
+            return MoveType.INSERT
+        return MoveType.ASK_AGAIN
+
+    def insert_stone(self, selected_triangle_index: int, selected_stone: Stone) -> None:
+        selected_triangle = self.triangles[selected_triangle_index]
+        selected_triangle.insert_stone(selected_stone)
+
+    def remove_stone(self, selected_triangle_index: int) -> None:
+        selected_triangle = self.triangles[selected_triangle_index]
+        selected_triangle.remove_stone()
+
+    def generate_dog_food(self, move_type: str, triangle_index: int, stone_value: int, is_over: bool) -> dict[str, str]:
+        return {
+            "move_type": move_type,
+            "triangle_index": str(triangle_index),
+            "stone_value": str(stone_value),
+            "is_over": str(is_over)
+        }
 
     def triangle_selected(self, triangle_index: int) -> dict[str, str]:
         # TODO: implementar cadeia de execuções para o caso de um triângulo válido ser selecionado (remover a pedra do
@@ -105,16 +155,8 @@ class Board:
         self.remote_player.toggle_turn()
         return player
 
-    def get_board(self) -> list[tuple[tuple[str, str] | None, tuple[str, str] | None]]:
-        board = []
-        for triangle in self.triangles:
-            board.append(
-                (
-                    self.stone_to_tuple(triangle.stone) if triangle.stone else None,
-                    self.stone_to_tuple(triangle.border_stone) if triangle.border_stone else None,
-                )
-            )
-        return board
+    def get_board(self) -> BoardInterface:
+        ...
 
     def stone_to_tuple(self, stone: Stone) -> tuple[str, str]:
         return str(stone.value), stone.color
