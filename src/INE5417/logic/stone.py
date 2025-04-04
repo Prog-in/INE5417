d93cd69154af2c77d84ddbcbd2e8119625dbd646
João Gabriel Feres <joaogabrielferes@gmail.com>
2025-04-04 00:10:09 -0300

    wip

--- src/INE5417/logic/stone.py
+++ src/INE5417/logic/stone.py
@@ -2,9 +2,16 @@ class Stone:
     def __init__(self, color: str, i: int):
         self.color: str = color
         self.value: int = i
+        self.on_board: bool = False
 
     def get_color(self) -> str:
         return self.color
 
     def get_value(self) -> int:
         return self.value
+
+    def set_on_board(self, on_board: bool) -> None:
+        self.on_board = on_board
+
+    def get_on_board(self) -> bool:
+        return self.on_board
--- src/INE5417/logic/triangle.py
+++ src/INE5417/logic/triangle.py
@@ -12,13 +12,15 @@ class Triangle:
     def insert_stone(self, stone: Stone):
         self.stone = stone
 
-    def remove_stone(self): 
+    def remove_stone(self) -> Stone:
         if self.stone is not None:
             self.border_stone = self.stone
+        tmp = self.stone
         self.stone = None
+        return tmp
 
     def get_border_stone(self) -> Stone:
         return self.border_stone
 
     def is_free(self):
-        return self.stone == None
+        return self.stone is None
