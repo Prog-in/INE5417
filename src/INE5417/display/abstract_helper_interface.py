5621975b518242da336d8a58487eb8e53397d5f5
João Gabriel Feres <joaogabrielferes@gmail.com>
2025-04-25 22:48:29 -0300

    wip

--- src/INE5417/display/abstract_helper_interface.py
+++ src/INE5417/display/abstract_helper_interface.py
@@ -11,11 +11,17 @@ class AbstractHelperInterface(ABC):
         self.root: tk.Tk = root
         self.assets: dict[str, tk.PhotoImage] = assets
         self.player_interface = player_interface
-        self.frame: ttk.Frame = self.initialize_frame()
+        self.frame: ttk.Frame | None = None
 
     @abstractmethod
     def initialize_frame(self) -> ttk.Frame:
         pass
 
-    def get_frame(self):
+    @abstractmethod
+    def update_widgets(self, new_assets: dict[str, tk.PhotoImage]) -> None:
+        pass
+
+    def get_frame(self) -> ttk.Frame:
+        if self.frame is None:
+            self.frame = self.initialize_frame()
         return self.frame
