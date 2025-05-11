2b2ebcf39b1c07d486e579ac50194f0cfcece187
Hélcio Valentim de Andrade Neto <valentimhelcio@gmail.com>
2025-05-11 16:08:27 -0300

    Pequenas mudanças

--- src/INE5417/logic/stone.py
+++ src/INE5417/logic/stone.py
@@ -2,7 +2,6 @@ class Stone:
     def __init__(self, i: int):
         self.color: str | None = None
         self.value: int = i
-        self.on_board: bool = False
 
     def update(self, color: str):
         self.color = color
@@ -12,9 +11,3 @@ class Stone:
 
     def get_value(self) -> int:
         return self.value
-
-    def set_on_board(self, on_board: bool) -> None:
-        self.on_board = on_board
-
-    def get_on_board(self) -> bool:
-        return self.on_board
