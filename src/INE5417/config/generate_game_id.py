3a35bfcfcfd6ea14f4c9fc0de47c1bf607e59417
João Gabriel Feres <joaogabrielferes@gmail.com>
2025-06-20 18:10:17 -0300

    Melhoria na geração do id da partida
    * Agora, o arquivo de geração do id da partida sempre é gerado no diretório adequado;
    
    * Explicado como gerar de id da partida no README.md.

--- src/INE5417/config/generate_game_id.py
+++ src/INE5417/config/generate_game_id.py
@@ -1,7 +1,9 @@
 from time import time
+from pathlib import Path
 
 milliseconds = int(time() * 1000)
 game_id = str(milliseconds - 1639872000000)
-file = open("game.id", "w")
+path = Path(__file__).parent
+file = open(path / "game.id", "w")
 file.write(game_id)
 file.close()
